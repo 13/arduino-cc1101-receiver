40a3f17bc47568782bb9ab863e964212380bf2fc
13 <eggerben@gmail.com>
2023-02-16 15:56:32 +0100

    Some fixes

--- src/my_credentials.h
+++ src/my_credentials.h
@@ -1,6 +1,8 @@
 // #define VERBOSE
 // #define DEBUG
+// #define DEBUG_CRC
 
-#define CC_FREQ 868.32
-#define CC_POWER 10
-#define GD0 2
+// #define GD0 2               // Disable for new method without GD0
+#define CC_FREQ 433.4e6
+#define CC_POWER 12
+#define CC_DELAY 200
