bfa01166e20c13e2683796ceb4b77354bbb45ff5
nofe4rz@gmail.com <nofe4rz@gmail.com>
2023-02-17 09:23:38 +0100

    Initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
-#include <ELECHOUSE_CC1101_SRC_DRV.h>
+#include <RadioLib.h>
 #include "credentials.h"
 
 // Edit credentials.h
 
 // cc1101
 const uint8_t byteArrSize = 61;
+CC1101 radio = new Module(10, GD0, RADIOLIB_NC, 3);
 
 #ifdef VERBOSE
 // one minute mark
@@ -40,21 +41,10 @@ void setup()
   Serial.println();
   // Start CC1101
   Serial.print(F("> [CC1101] Initializing... "));
-  int cc_state = ELECHOUSE_cc1101.getCC1101();
-  if (cc_state)
+  int cc_state = radio.begin(CC_FREQ);
+  if (cc_state == RADIOLIB_ERR_NONE)
   {
     Serial.println(F("OK"));
-    ELECHOUSE_cc1101.Init(); // must be set to initialize the cc1101!
-#ifdef GD0
-    ELECHOUSE_cc1101.setGDO0(GD0); // set lib internal gdo pin (gdo0). Gdo2 not use for this example.
-#endif
-    ELECHOUSE_cc1101.setCCMode(1);     // set config for internal transmission mode.
-    ELECHOUSE_cc1101.setModulation(0); // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.
-    ELECHOUSE_cc1101.setMHZ(CC_FREQ);  // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
-    // ELECHOUSE_cc1101.setPA(CC_POWER);  // Set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12) Default is max!
-    ELECHOUSE_cc1101.setSyncMode(2);   // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.
-    ELECHOUSE_cc1101.setCrc(1);        // 1 = CRC calculation in TX and CRC check in RX enabled. 0 = CRC disabled for TX and RX.
-    ELECHOUSE_cc1101.setCRC_AF(1);     // Enable automatic flush of RX FIFO when CRC is not OK. This requires that only one packet is in the RXIFIFO and that packet length is limited to the RX FIFO size.
   }
   else
   {
@@ -70,60 +60,27 @@ void loop()
 #ifdef MARK
   printMARK();
 #endif
-#ifdef GD0
-  if (ELECHOUSE_cc1101.CheckReceiveFlag())
-#else
-  if (ELECHOUSE_cc1101.CheckRxFifo(CC_DELAY))
-#endif
+  byte byteArr[byteArrSize] = {0};
+  int state = radio.receive(byteArr, byteArrSize);
+  if (state == RADIOLIB_ERR_NONE)
   {
-    byte byteArr[byteArrSize] = {0};
-#ifdef DEBUG
+
+#ifdef VERBOSE
     Serial.print(F("> [CC1101] Receive... "));
-#endif
-    if (ELECHOUSE_cc1101.CheckCRC())
-    {
-#ifdef VERBOSE
-#ifndef DEBUG
-      Serial.print(F("> [CC1101] Receive... "));
-#endif
-#endif
-#ifdef VERBOSE
-      Serial.print(F("CRC "));
-#endif
-      int byteArrLen = ELECHOUSE_cc1101.ReceiveData(byteArr);
+    Serial.print(F("CRC "));
+    Serial.println(F("OK"));
 
-#ifdef VERBOSE
-      Serial.println(F("OK"));
-
-      Serial.print(F("> [CC1101] Length: "));
-      Serial.println(byteArrLen);
+    Serial.print(F("> [CC1101] Length: "));
+    Serial.println(sizeof(byteArr));
 #endif
-      // byteArr[byteArrLen] = '0';
-      for (uint8_t i = 0; i < byteArrLen; i++)
-      {
-        Serial.print((char)byteArr[i]);
-      }
-      Serial.print(F(",RSSI:"));
-      Serial.println(ELECHOUSE_cc1101.getRssi());
-      // Serial.print(F(",LQI:"));
-      // Serial.println(ELECHOUSE_cc1101.getLqi());
-    }
-#ifdef DEBUG
-    else
+    for (uint8_t i = 0; i < sizeof(byteArr); i++)
     {
-      Serial.println(F("ERR CRC"));
-#ifdef DEBUG_CRC
-      for (uint8_t i = 0; i < byteArrSize; i++)
-      {
-        Serial.print((char)byteArr[i]);
-      }
-      Serial.print(F(",RSSI:"));
-      Serial.println(ELECHOUSE_cc1101.getRssi());
-      // Serial.print(F(",LQI:"));
-      // Serial.println(ELECHOUSE_cc1101.getLqi());
-#endif
+      Serial.print((char)byteArr[i]);
     }
-#endif
+    Serial.print(F(",RSSI:"));
+    Serial.print(radio.getRSSI());
+    Serial.print(F(",LQI:"));
+    Serial.println(radio.getLQI());
   }
 }
 
