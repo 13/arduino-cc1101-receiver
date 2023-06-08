3bbc82f5369af81fed87037b5d29be47343b6589
13 <13@users.noreply.github.com>
2023-06-08 06:10:55 +0200

    added wstation config

--- src/credentials_example.h
+++ src/credentials_example.h
@@ -1,10 +1,21 @@
+#define WSTATION
 #define VERBOSE
 // #define DEBUG
 // #define DEBUG_CRC
 
+#if defined(WSTATION)
+#define CC_FREQ 868.3
+#define CC_BR 8.21
+#define CC_FD 57.136417
+#define CC_RxBW 270
+#define CC_SM 3 // 3=30/32 7=30/32+threshold
+#define CC_POWER 10
+#define CC_DELAY 200
+#else
 #define CC_FREQ 868.32
 #define CC_POWER 12
 #define CC_DELAY 200
+#endif
 #if defined(ESP8266)
 // WiFi
 const char *wifi_ssid = "";
