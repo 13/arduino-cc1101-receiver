fe120c1b0e57e4e288609919c08a586a5e987843
13 <eggerben@gmail.com>
2023-12-04 22:02:34 +0100

    update credentials example

--- src/credentials_example.h
+++ src/credentials_example.h
@@ -5,7 +5,7 @@
 #define CC_FREQ 868.32
 #define CC_POWER 12
 #define CC_DELAY 200
-#if defined(ESP8266)
+
 /* WiFi */
 const char* wifi_ssid = "network";
 const char* wifi_pass  = "";
@@ -18,13 +18,3 @@ const char* mqtt_topic_lwt = "muh/esp";
 uint16_t mqtt_port = 1883;
 // #define GD0 5 /* Bug with ESP8266 */
 #define WSPACKETS
-#define MAX_SENSOR_DATA 5
-#endif
-
-// GIT
-#ifndef GIT_VERSION
-#define GIT_VERSION "unknown"
-#endif
-#ifndef GIT_VERSION_SHORT
-#define GIT_VERSION_SHORT "0"
-#endif
